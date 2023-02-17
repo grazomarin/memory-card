11e7cf855c7e46f8ed99412fa9e0fc7837faa8da
grazomarin <huseynovk355@gmail.com>
2023-02-17 22:27:48 +0400

    feat(board.jsx): add level state, add restart and gameover functions
    board renders a specific number of cards based on game level.

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,14 +1,38 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Card from './Card';
+import uniqid from 'uniqid';
+import Gameover from './Gameover';
 
 const Board = () => {
+	const [level, setLevel] = useState(1);
+	const [gameover, setGameover] = useState(false);
+
+	const restartGame = () => {
+		setGameover(false);
+	};
+
+	const enableGameover = () => {
+		setGameover(true);
+	};
+
+	const renderLevel = () => {
+		let card = [];
+		for (let i = 0; i < 2 + 2 * level; i++) {
+			card.push(
+				<Card
+					key={uniqid()}
+					id={uniqid()}
+					enableGameover={enableGameover}
+				/>
+			);
+		}
+		return card;
+	};
+
 	return (
 		<div className="board">
-			<Card />
-			<Card />
-			<Card />
-			<Card />
-			<Card />
+			{renderLevel()}
+			{gameover && <Gameover restartGame={restartGame} />}
 		</div>
 	);
 };

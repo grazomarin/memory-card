8c0f98e782bf9f7c77626d74e34c787e7d44a8f5
grazomarin <huseynovk355@gmail.com>
2023-02-17 22:25:10 +0400

    feat(gameover.jsx): add basic gamover screen
    on restart button press game restarts

--- src/components/Gameover.jsx
+++ src/components/Gameover.jsx
@@ -1,5 +1,15 @@
 import React, { useState } from 'react';
 
-const Gameover = () => {};
+const Gameover = (props) => {
+	return (
+		<div className="background">
+			<div className="gameover">
+				<button className="restart" onClick={props.restartGame}>
+					Restart Game
+				</button>
+			</div>
+		</div>
+	);
+};
 
 export default Gameover;

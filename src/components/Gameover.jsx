1b62d11bbc522ce6123de9b1415cffc4bcdf7d5e
grazomarin <huseynovk355@gmail.com>
2023-02-21 23:27:14 +0400

    feat(gameover.jsx index.jsx): add score to gameover screen, style gameover screen

--- src/components/Gameover.jsx
+++ src/components/Gameover.jsx
@@ -1,9 +1,13 @@
 import React, { useState } from 'react';
+import { useScore } from './contexts/ScoresContext';
 
 const Gameover = (props) => {
+	const score = useScore();
+
 	return (
 		<div className="background">
 			<div className="gameover">
+				<div className="gameover_score">Score: {score}</div>
 				<button className="restart" onClick={props.restartGame}>
 					Play Again
 				</button>

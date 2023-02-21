1a59de33325ef7afb5d02c67463c48fba9cb9a97
grazomarin <huseynovk355@gmail.com>
2023-02-21 23:38:06 +0400

    feat(header.jsx, index.scss): add mobile support

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,26 @@
-import React, { useState } from 'react';
-import Score from './Score';
+import React, { useEffect, useState } from 'react';
+import useWindowDimensions from './hooks/WindowDimensionsHook';
+import Status from './Status';
 import logo from '../assets/images/logo.svg';
 
 const Header = () => {
+	const [title, setTitle] = useState();
+	const { height, width } = useWindowDimensions();
+
+	useEffect(() => {
+		width < 900 && width > 500
+			? setTitle('CMC')
+			: setTitle('Colors Memory Card');
+		width;
+	}, [width]);
+
 	return (
 		<div id="header">
-			<h2>Colors Memory Card</h2>
-			<Score />
-			<img src={logo} alt="" className="logo" />
+			<h2 className="title">{title}</h2>
+			<Status />
+			<a href="https://github.com/grazomarin/memory-card" target="_blank">
+				<img src={logo} alt="" className="logo" />
+			</a>
 		</div>
 	);
 };
